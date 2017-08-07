3416927151e2576827bf7ba6f8123a1ccf53861d
William Hatcher <wjeffhatcher@gmail.com>
2017-08-07 01:51:54 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemName = item;
+  var itemConstructor = new Object();
+  itemConstructor[itemName] = Math.floor(Math.random()*50);
+  cart.push(itemConstructor)
+  console.log(`${itemName} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  var text = 'In your cart, you have '
+  if(cart.length === 0){
+    return `Your shopping cart is empty.`}
+  for(var i = 0; i<cart.length; i++ ){
+    var obj = cart[i]
+    var values = Object.keys(cart[i])
+    if(i === cart.length-1) {
+      text +=  'and ' + values + " at $" + obj[values] + '.'
+    }else{
+      text += values + " at $" + obj[values] + ', '
+     } }return text
 }
 
 function total() {
